35cef8082971f2b4c57ee089f8179a63bc1de0b7
Aakil Fernandes <aakilfernandes@gmail.com>
2019-12-18 18:27:24 -0500

    cleanup node

--- node/constsants.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.PERSONAL_MESSAGE_PREFIX_ASCII = '\x19Ethereum Signed Message:\n189Alchemilla V1 Order:\n';
--- node/x.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ORDER_TYPE;
-(function (ORDER_TYPE) {
-    ORDER_TYPE[ORDER_TYPE["BUYY"] = 0] = "BUYY";
-    ORDER_TYPE[ORDER_TYPE["SELL"] = 1] = "SELL";
-})(ORDER_TYPE = exports.ORDER_TYPE || (exports.ORDER_TYPE = {}));
