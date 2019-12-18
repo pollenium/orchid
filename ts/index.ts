f152712a62559fc0530d88486283861bb9b34e1c
Aakil Fernandes <aakilfernandes@gmail.com>
2019-12-18 14:56:25 -0500

    export constants and enums

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,8 @@
 import * as _constants from './constants'
+import * as _enums from './enums'
 
+export const constants = _constants
+export const enums = _enums
 export { Order } from './classes/Order'
 export { SignedOrder } from './classes/SignedOrder'
 export { OrderPair } from './classes/OrderPair'
